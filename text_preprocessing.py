5f7d0d564f98675c0d4e97eb6e992d3ac57d2eac
shyamsparrow <47350895+shyamsparrow@users.noreply.github.com>
2021-11-23 19:30:16 +0530

    Update text_preprocessing.py

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -321,4 +321,4 @@ def __init__(to_lower, to_upper, remove_number, remove_itemized_bullet_and_numbe
              expand_contraction, normalize_unicode, remove_stopword, remove_email,
              remove_phone_number, remove_ssn, remove_credit_card_number, remove_name,
              check_spelling, tokenize_word, tokenize_sentence, stem_word, lemmatize_word,
-             substitute_token, preprocess_text):
+             substitute_token, preprocess_text)
