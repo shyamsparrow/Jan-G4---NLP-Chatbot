fa0b85200bc959b0b3611c06256b3f832ec1de6f
shyamsparrow <47350895+shyamsparrow@users.noreply.github.com>
2021-11-23 18:27:08 +0530

    Update text_preprocessing.py

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -321,4 +321,20 @@ def __init__(to_lower, to_upper, remove_number, remove_itemized_bullet_and_numbe
              expand_contraction, normalize_unicode, remove_stopword, remove_email,
              remove_phone_number, remove_ssn, remove_credit_card_number, remove_name,
              check_spelling, tokenize_word, tokenize_sentence, stem_word, lemmatize_word,
-             substitute_token, preprocess_text)
+             substitute_token, preprocess_text):
+    
+
+%%writefile config.py
+to_lower, 
+remove_url,
+remove_punctuation,
+remove_special_character,
+keep_alpha_numeric, 
+remove_whitespace,
+remove_stopword,
+check_spelling, 
+tokenize_word, 
+tokenize_sentence, 
+stem_word,
+lemmatize_word,
+substitute_token, 
