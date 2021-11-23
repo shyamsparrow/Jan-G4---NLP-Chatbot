552a430bca3147fcebc970ad636984547077b11f
shyamsparrow <47350895+shyamsparrow@users.noreply.github.com>
2021-11-23 17:23:13 +0530

    Update text_preprocessing.py

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -316,3 +316,9 @@ def preprocess_text(input_text: str, processing_function_list: Optional[List[Cal
         processed_text = ' '.join(input_text)
     return processed_text
 
+def __init__(to_lower, to_upper, remove_number, remove_itemized_bullet_and_numbering, remove_url,
+             remove_punctuation, remove_special_character, keep_alpha_numeric, remove_whitespace,
+             expand_contraction, normalize_unicode, remove_stopword, remove_email,
+             remove_phone_number, remove_ssn, remove_credit_card_number, remove_name,
+             check_spelling, tokenize_word, tokenize_sentence, stem_word, lemmatize_word,
+             substitute_token, preprocess_text)
